a37eaa6a04a5911191c40569ec85c4ed7d1c411e
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2021-12-22 09:53:54 +0000

    Update 01122021.sql

--- WS/01122021.sql
+++ WS/01122021.sql
@@ -1,13 +1,5 @@
--- Expiry bill
-update egbs_billdetail_v1 set expirydate ='1638057599000'
-where id in (select ebv.id from egbs_billdetail_v1 ebv
-		inner join egbs_bill_v1 ebv2 on ebv.billid=ebv2.id
-	where ebv2.status ='ACTIVE' 
-		and consumercode in (select edv.consumercode from egbs_demand_v1 edv
-			where edv.tenantid ='od.cuttack' and edv.businessservice ='WS' and edv.ispaymentcompleted =false and edv.taxperiodfrom > 1635642000000 and edv.taxperiodfrom < 1635858000000))
-
--- Update WS_SPECIAL_REBATE to WS_TIME_REBATE for Nov, 2021 demand
-update egbs_demanddetail_v1 set taxheadcode='WS_TIME_REBATE'
-	where id in (select id from egbs_demanddetail_v1 edv 
-			where demandid in (select id from egbs_demand_v1 edv where tenantid ='od.cuttack' and businessservice ='WS' and ispaymentcompleted =false and taxperiodfrom > 1635642000000 and taxperiodfrom < 1635858000000)
-					and taxheadcode='WS_SPECIAL_REBATE');
+egov.demand.tenants=ALL
+egov.demand.tenants.skip=od.bijepur,od.odagaon,od.dhamnagar,od.nilgiri,od.belpahar,od.chandbali,od.pipli,od.hindol,od.tusura,od.dharamgarh,od.gudari,od.nuapada,od.balimela,od.konark,od.brajrajnagar,od.gopalpur,od.tarbha,od.kesinga,od.malkangiri,od.khandapada,od.umerkote,od.khallikote,od.balliguda,od.udala,od.basudevpur,od.daspalla,od.champua,od.padampur,od.chikiti,od.kamakhyanagar,od.karanjia,od.athmallik,od.banki,od.kotpad,od.junagarh,od.soro,od.rambha,od.birmitrapur,od.ganjam,od.kuchinda,od.bellaguntha,od.banpur,od.attabira,od.athagarh,od.balugaon,od.barbil,od.aska,od.barpali,od.angul,od.anandapur,od.bargarh,od.balangir,od.balasore,od.baripada,od.puri,od.berhampur,od.rourkela,od.cuttack,od.sambalpur,od.bhubaneswar
+egov.demand.manualstartenddate.enabled=true
+egov.demand.manualmonthnumber=11
+egov.demand.manualyear=2021
