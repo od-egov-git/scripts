dc3b02bf60578428fdf663e182009a04a3875e68
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-01-14 13:07:23 +0000

    Update bpa.sql

--- bpa.sql
+++ bpa.sql
@@ -1,7 +1,18 @@
--- Insert the workflow
--- Assigner keep same as previous
-INSERT INTO eg_wf_processinstance_v2 (id,tenantid,businessservice,businessid,"action",status,"comment",assigner,assignee,statesla,previousstatus,createdby,lastmodifiedby,createdtime,lastmodifiedtime,modulename,businessservicesla,rating) values
-('1cd7071b-6854-4ce4-8821-c642d08f37d9','od.rourkela','BPA1','BP-RRK-2021-12-15-000197','PAY','a5bb57a7-5154-4d3f-a94f-a7d2db265dd5',NULL,'0d2957d0-4736-4b49-8640-1605e5d0a041',NULL,NULL,NULL,'0d2957d0-4736-4b49-8640-1605e5d0a041','0d2957d0-4736-4b49-8640-1605e5d0a041',1640329362000,1640329362000,'bpa-services',5181630506,NULL);
+-- Update application status
+update eg_bpa_buildingplan set status='APPROVED' where applicationno ='BP-RRK-2021-11-16-000159';
 
--- Update application
-update eg_bpa_buildingplan set status='DOC_VERIFICATION_INPROGRESS' where applicationno ='BP-RRK-2021-12-15-000197';
+-- Update demand with collection amount
+update egbs_demand_v1 set ispaymentcompleted =true where consumercode ='BP-RRK-2021-11-16-000159' and businessservice ='BPA.NC_SAN_FEE';
+update egbs_demanddetail_v1 set collectionamount =taxamount where demandid =(select id from egbs_demand_v1 where consumercode='BP-RRK-2021-11-16-000159' and businessservice ='BPA.NC_SAN_FEE');
+
+
+-- Update application status
+update eg_bpa_buildingplan set status='APPROVED' where applicationno ='BP-RRK-2021-12-09-000189';
+
+-- Insert the workflow
+INSERT INTO eg_wf_processinstance_v2 (id,tenantid,businessservice,businessid,"action",status,"comment",assigner,assignee,statesla,previousstatus,createdby,lastmodifiedby,createdtime,lastmodifiedtime,modulename,businessservicesla,rating) values
+('aa8f62fb-ecc1-42f8-a5ff-d9008803a4c0','od.rourkela','BPA1','BP-RRK-2021-12-09-000189','PAY','373b81d4-cc81-4400-8d4b-5d32dd722bef',NULL,'cbf66c9d-c8c3-4ba3-b77d-791ceadae440',NULL,NULL,NULL,'cbf66c9d-c8c3-4ba3-b77d-791ceadae440','cbf66c9d-c8c3-4ba3-b77d-791ceadae440',1641020562000,1641020562000,'bpa-services',5181630506,NULL);
+
+-- Update demand with collection amount
+update egbs_demand_v1 set ispaymentcompleted =true where consumercode ='BP-RRK-2021-12-09-000189' and businessservice ='BPA.NC_SAN_FEE';
+update egbs_demanddetail_v1 set collectionamount =taxamount where demandid =(select id from egbs_demand_v1 where consumercode='BP-RRK-2021-12-09-000189' and businessservice ='BPA.NC_SAN_FEE');
