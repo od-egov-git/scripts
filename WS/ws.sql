b04d127729871ed5ec17bcae4e60b2e2bdf6d3b2
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-02-02 11:52:18 +0000

    Update ws.sql

--- WS/ws.sql
+++ WS/ws.sql
@@ -1,4 +1,19 @@
--- WS_AP/HNL/2021-22/2173625
--- Activate connection and Pay action ording mismatch fix.
+-- Meter reading to delete for OTHERS usageCategory logged from 9-SEP-2021
 
-update eg_wf_processinstance_v2 set createdtime='1642573848000', lastmodifiedtime='1642573848000' where id='256838fb-598d-450f-bce1-d386b530ff1e';
+delete from eg_ws_meterreading where
+id not in (select distinct on (connectionno) id from eg_ws_meterreading order by connectionno, currentreadingdate)
+and connectionno in (select distinct connectionno from eg_ws_connection where id in (
+select connection_id from eg_ws_service where usagecategory='OTHERS')
+and isoldapplication=false and applicationstatus='CONNECTION_ACTIVATED')
+and date(to_timestamp(currentreadingdate/1000)) >= to_date('2021-09-09', 'yyyy-MM-dd');
+
+
+-- Update OTHERS to BPL
+
+update eg_ws_service set usagecategory='BPL' where connection_id in (
+select ews.connection_id
+from eg_ws_connection ewc
+	inner join eg_ws_service ews on ews.connection_id=ewc.id
+where ews.usagecategory='OTHERS'
+	and ewc.isoldapplication=false
+	and ewc.applicationstatus='CONNECTION_ACTIVATED');
