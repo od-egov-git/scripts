e87c5f2611cb494bd1af25cc926aee0207d81a22
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-02-12 15:43:22 +0000

    Update ws.sql

--- WS/ws.sql
+++ WS/ws.sql
@@ -1,23 +1,17 @@
---WS/BMC/1574832--
+-- Duplicate Demand Details Deletion Query For Bhubaneswar--
 
--- Update demand to payment completed
-update egbs_demand_v1
-set ispaymentcompleted=true
-where id in ('87b63ba1-a047-4268-936e-9a3169e71bd1',
-			'040ef8d7-49e7-41c6-ab21-0d9f150b5de3',
-			'bf4b2c76-5538-4c71-9d40-28d3b3eccc54',
-			'4f3c0251-d4c6-4f89-8c81-0566e68ce16b');
+delete from egbs_demanddetail_v1 edv where demandid in (
+select id from egbs_demand_v1 edv2 where id not in (
+select distinct on (consumercode, date(to_timestamp(taxperiodfrom/1000)), date(to_timestamp(taxperiodto/1000))) id
+from egbs_demand_v1 edv where businessservice='WS' and tenantid ='od.bhubaneswar'
+order by consumercode, date(to_timestamp(taxperiodfrom/1000)), date(to_timestamp(taxperiodto/1000)), createdtime)
+and businessservice ='WS' and tenantid ='od.bhubaneswar');
 
--- Update time rebate to adjusted amount
-update egbs_demanddetail_v1
-set taxamount=-6
-where demandid='4f3c0251-d4c6-4f89-8c81-0566e68ce16b'
-and taxheadcode='WS_TIME_REBATE';
 
--- update collected amount
-update egbs_demanddetail_v1
-set collectionamount=taxamount
-where demandid in ('87b63ba1-a047-4268-936e-9a3169e71bd1',
-			'040ef8d7-49e7-41c6-ab21-0d9f150b5de3',
-			'bf4b2c76-5538-4c71-9d40-28d3b3eccc54',
-			'4f3c0251-d4c6-4f89-8c81-0566e68ce16b');
+-- Duplicate Demand Details Deletion Query For Bhubaneswar--
+
+delete from egbs_demand_v1 edv2 where id not in (
+select distinct on (consumercode, date(to_timestamp(taxperiodfrom/1000)), date(to_timestamp(taxperiodto/1000))) id
+from egbs_demand_v1 edv where businessservice='WS' and tenantid ='od.bhubaneswar'
+order by consumercode, date(to_timestamp(taxperiodfrom/1000)), date(to_timestamp(taxperiodto/1000)), createdtime)
+and businessservice ='WS' and tenantid ='od.bhubaneswar';
