e9637f920df1e949f1e891d27025b429c2f34cb0
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-02-13 07:38:39 +0000

    Update ws.sql

--- WS/ws.sql
+++ WS/ws.sql
@@ -1,17 +1,17 @@
--- Duplicate Demand Details Deletion Query For Bhubaneswar--
+-- Duplicate Demand Details Deletion Query--
 
 delete from egbs_demanddetail_v1 edv where demandid in (
 select id from egbs_demand_v1 edv2 where id not in (
 select distinct on (consumercode, date(to_timestamp(taxperiodfrom/1000)), date(to_timestamp(taxperiodto/1000))) id
-from egbs_demand_v1 edv where businessservice='WS' and tenantid ='od.bhubaneswar'
+from egbs_demand_v1 edv where businessservice='WS'
 order by consumercode, date(to_timestamp(taxperiodfrom/1000)), date(to_timestamp(taxperiodto/1000)), createdtime)
-and businessservice ='WS' and tenantid ='od.bhubaneswar');
+and businessservice ='WS');
 
 
--- Duplicate Demand Details Deletion Query For Bhubaneswar--
+-- Duplicate Demand Deletion Query For Bhubaneswar--
 
 delete from egbs_demand_v1 edv2 where id not in (
 select distinct on (consumercode, date(to_timestamp(taxperiodfrom/1000)), date(to_timestamp(taxperiodto/1000))) id
-from egbs_demand_v1 edv where businessservice='WS' and tenantid ='od.bhubaneswar'
+from egbs_demand_v1 edv where businessservice='WS'
 order by consumercode, date(to_timestamp(taxperiodfrom/1000)), date(to_timestamp(taxperiodto/1000)), createdtime)
-and businessservice ='WS' and tenantid ='od.bhubaneswar';
+and businessservice ='WS';
