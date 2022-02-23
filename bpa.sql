3bc105a32fe25494678f43e99e16f37e76eb91a0
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-02-23 14:48:12 +0000

    Update bpa.sql

--- bpa.sql
+++ bpa.sql
@@ -1,49 +1,21 @@
---BPR-2021-10-30-000566--
 
-delete from eg_tl_Accessory where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2021-10-30-000566'));
-delete from eg_tl_address where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2021-10-30-000566'));
-delete from eg_tl_ApplicationDocument where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2021-10-30-000566'));
-delete from eg_tl_document_owner where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2021-10-30-000566'));
-delete from eg_tl_dscdetails etd where tradelicensedetailid =(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2021-10-30-000566'));
-delete from eg_tl_institution where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2021-10-30-000566'));
-delete from eg_tl_owner where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2021-10-30-000566'));--0 row
-delete from eg_tl_TradeUnit where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2021-10-30-000566'));
+INSERT INTO eg_wf_processinstance_v2 (id,tenantid,businessservice,businessid,"action",status,"comment",assigner,assignee,statesla,previousstatus,createdby,lastmodifiedby,createdtime,lastmodifiedtime,modulename,businessservicesla,rating) values
+('d0083eaa-079e-49e1-baac-43c3bbc30032','od.bhubaneswardevelopmentauthority','BPA1','BP-BDA-2022-01-29-000578','PAY','a5bb57a7-5154-4d3f-a94f-a7d2db265dd5',NULL,'4367b4e4-46a6-4bd1-bcd1-fd5a4b9c29c3',NULL,NULL,NULL,'4367b4e4-46a6-4bd1-bcd1-fd5a4b9c29c3','4367b4e4-46a6-4bd1-bcd1-fd5a4b9c29c3',1643439750295,1643439750295,'bpa-services',5181630506,NULL);
 
-delete from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2021-10-30-000566');
-delete from eg_tl_tradelicense where applicationNumber='BPR-2021-10-30-000566';
+-- Update application
+update eg_bpa_buildingplan set status='DOC_VERIFICATION_INPROGRESS' where applicationno ='BP-BDA-2022-01-29-000578';
 
-delete from eg_wf_processinstance_v2 ewpv where businessid ='BPR-2021-10-30-000566';
+==============
+INSERT INTO eg_wf_processinstance_v2 (id,tenantid,businessservice,businessid,"action",status,"comment",assigner,assignee,statesla,previousstatus,createdby,lastmodifiedby,createdtime,lastmodifiedtime,modulename,businessservicesla,rating) values
+('fa819a21-f4f0-4af4-bb2f-f56b0c2f9510','od.rourkela','BPA1','BP-RRK-2022-01-29-000575','PAY','a5bb57a7-5154-4d3f-a94f-a7d2db265dd5',NULL,'c03bf043-7a29-4b74-b4de-6d3a2047ada6',NULL,NULL,NULL,'c03bf043-7a29-4b74-b4de-6d3a2047ada6','c03bf043-7a29-4b74-b4de-6d3a2047ada6',1643439135304,1643439135304,'bpa-services',5181630506,NULL);
 
-=============
---BPR-2022-01-10-001059--
+-- Update application
+update eg_bpa_buildingplan set status='DOC_VERIFICATION_INPROGRESS' where applicationno ='BP-RRK-2022-01-29-000575';
 
-delete from eg_tl_Accessory where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-10-001059'));
-delete from eg_tl_address where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-10-001059'));
-delete from eg_tl_ApplicationDocument where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-10-001059'));
-delete from eg_tl_document_owner where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-10-001059'));
-delete from eg_tl_dscdetails etd where tradelicensedetailid =(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-10-001059'));
-delete from eg_tl_institution where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-10-001059'));
-delete from eg_tl_owner where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-10-001059'));--0 row
-delete from eg_tl_TradeUnit where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-10-001059'));
+==================
 
-delete from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-10-001059');
-delete from eg_tl_tradelicense where applicationNumber='BPR-2022-01-10-001059';
+INSERT INTO eg_wf_processinstance_v2 (id,tenantid,businessservice,businessid,"action",status,"comment",assigner,assignee,statesla,previousstatus,createdby,lastmodifiedby,createdtime,lastmodifiedtime,modulename,businessservicesla,rating) values
+('84d23825-3c8b-4822-b5df-d8483f2fb463','od.bhubaneswar','BPA1','BP-BMC-2022-01-24-000518','PAY','a5bb57a7-5154-4d3f-a94f-a7d2db265dd5',NULL,'b9541811-fda4-4164-840a-f6e78c98b83a',NULL,NULL,NULL,'b9541811-fda4-4164-840a-f6e78c98b83a','b9541811-fda4-4164-840a-f6e78c98b83a',1643009504501,1643009504501,'bpa-services',5181630506,NULL);
 
-delete from eg_wf_processinstance_v2 ewpv where businessid ='BPR-2022-01-10-001059';
-
-=================
---BPR-2022-01-02-000769--
-
-delete from eg_tl_Accessory where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-02-000769'));
-delete from eg_tl_address where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-02-000769'));
-delete from eg_tl_ApplicationDocument where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-02-000769'));
-delete from eg_tl_document_owner where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-02-000769'));
-delete from eg_tl_dscdetails etd where tradelicensedetailid =(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-02-000769'));
-delete from eg_tl_institution where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-02-000769'));
-delete from eg_tl_owner where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-02-000769'));--0 row
-delete from eg_tl_TradeUnit where tradeLicenseDetailId=(select id from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-02-000769'));
-
-delete from eg_tl_TradeLicenseDetail where tradelicenseId=(select id from eg_tl_tradelicense where applicationNumber='BPR-2022-01-02-000769');
-delete from eg_tl_tradelicense where applicationNumber='BPR-2022-01-02-000769';
-
-delete from eg_wf_processinstance_v2 ewpv where businessid ='BPR-2022-01-02-000769';
+-- Update application
+update eg_bpa_buildingplan set status='DOC_VERIFICATION_INPROGRESS' where applicationno ='BP-BMC-2022-01-24-000518';
