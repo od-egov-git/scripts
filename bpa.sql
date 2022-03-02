3478d827aec95181ecbdaf5f339209175418ff67
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-03-02 07:36:14 +0000

    Update bpa.sql

--- bpa.sql
+++ bpa.sql
@@ -1,1 +1,1 @@
-delete from public.eg_bpa_document where id='57025645-0134-4eb8-ad17-c7d38b9ef4f5' and documenttype ='APPL_IDENTITYPROOF';
+delete  from public.eg_bpa_document ebd where buildingplanid =(select id from public.eg_bpa_buildingplan where applicationno ='BP-SMB-2021-12-12-000194') and documenttype ='APPL_OWNERIDPROOF';
