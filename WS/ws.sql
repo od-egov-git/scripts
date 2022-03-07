245f64b450e998de1c1c1bac7df9f480eaa22059
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-03-07 15:03:53 +0000

    Update ws.sql

--- WS/ws.sql
+++ WS/ws.sql
@@ -1,7 +1,8 @@
--- update connection execution date for migrated connections and set to 1st Aug 
-
-update eg_ws_service
-set connectionexecutiondate=1627815600000
-where connection_id in (select id from eg_ws_connection
-where oldconnectionno is not null)
-and connectionexecutiondate>1627815600000;
+delete from eg_ws_meterreading mrr where id in (
+select id from eg_ws_meterreading mr1
+inner join
+(select m1.connectionno, max(m1.currentreadingdate) as curr  from eg_ws_meterreading m1
+where m1.connectionno in (
+'WS/BMC/1551791','WS/BMC/1498972','WS/BMC/1551768','WS/BMC/1504750'
+) group by m1.connectionno) mr2
+on mr1.connectionno=mr2.connectionno and mr1.currentreadingdate=mr2.curr);
