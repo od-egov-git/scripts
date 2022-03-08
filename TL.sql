6319aeb2511f86235095253dd957b84697543195
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-03-08 09:00:21 +0000

    Create TL.sql

---
+++ TL.sql
@@ -0,0 +1,29 @@
+update eg_tl_billingslab set rate=5000 where tenantid='od.odagaon' and tradetype='NONEATINGNONMEDICALESTABLISHMENTS.TST1'
+
+update eg_tl_billingslab set rate=2000 where tenantid='od.birmitrapur' and applicationtype='NEW' 
+and tradetype='NONEATINGNONMEDICALESTABLISHMENTS.TST27' and fromuom=0
+
+update eg_tl_billingslab set rate=3000 where tenantid='od.birmitrapur' and applicationtype='NEW' 
+and tradetype='NONEATINGNONMEDICALESTABLISHMENTS.TST27' and fromuom=501
+
+update eg_tl_billingslab set rate=10000 where tenantid='od.birmitrapur' and applicationtype='NEW' 
+and tradetype='NONEATINGNONMEDICALESTABLISHMENTS.TST27' and fromuom=1001
+
+update eg_tl_billingslab set rate=3000 where tenantid='od.birmitrapur' and applicationtype='NEW' 
+and tradetype='NONEATINGNONMEDICALESTABLISHMENTS.TST26' and fromuom=0
+
+update eg_tl_billingslab set rate=5000 where tenantid='od.birmitrapur' and applicationtype='NEW' 
+and tradetype='NONEATINGNONMEDICALESTABLISHMENTS.TST26' and fromuom=1001
+
+update eg_tl_billingslab set rate=7000 where tenantid='od.birmitrapur' and applicationtype='NEW' 
+and tradetype='NONEATINGNONMEDICALESTABLISHMENTS.TST26' and fromuom=3001
+
+update eg_tl_billingslab set rate=1500  where tenantid='od.dhenkanal'  
+and tradetype='NONEATINGNONMEDICALESTABLISHMENTS.TST26'
+
+update eg_tl_billingslab set rate=1500  where tenantid='od.dhenkanal'  
+and tradetype='MEDICALESTABLISHMENTS.TST2'
+
+
+
+
