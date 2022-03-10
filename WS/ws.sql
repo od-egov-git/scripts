517ad3b793289f65730c28c839a61333b051f16a
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-03-10 05:06:33 +0000

    Update ws.sql

--- WS/ws.sql
+++ WS/ws.sql
@@ -1,8 +1,7 @@
-delete from eg_ws_meterreading mrr where id in (
-select id from eg_ws_meterreading mr1
-inner join
-(select m1.connectionno, max(m1.currentreadingdate) as curr  from eg_ws_meterreading m1
-where m1.connectionno in (
-'WS/BMC/1551791','WS/BMC/1498972','WS/BMC/1551768','WS/BMC/1504750'
-) group by m1.connectionno) mr2
-on mr1.connectionno=mr2.connectionno and mr1.currentreadingdate=mr2.curr);
+-- 0 Meter reading insert
+INSERT INTO public.eg_ws_meterreading
+(id, connectionno, billingperiod, meterstatus, lastreading, lastreadingdate, currentreading, currentreadingdate, consumption, createdby, lastmodifiedby, createdtime, lastmodifiedtime, tenantid) VALUES
+('b82cfaaa-0d53-4135-82c8-e4fd1663b1b0', 'WS/BMC/1504750', '01/11/2021 - 01/11/2021', 'Working', 0, 1635677000000, 0, 1635678000000, 0, '5c4504e7-40e9-47e9-850e-1b33e699b879', '5c4504e7-40e9-47e9-850e-1b33e699b879', 1637119904010, 1637119904010, 'od.bhubaneswar'),
+('5d4ebbf2-4035-470e-b490-f97ac728e92b', 'WS/BMC/1551768', '01/11/2021 - 01/11/2021', 'Working', 0, 1635677000000, 0, 1635678000000, 0, '5c4504e7-40e9-47e9-850e-1b33e699b879', '5c4504e7-40e9-47e9-850e-1b33e699b879', 1637119904010, 1637119904010, 'od.bhubaneswar'),
+('e6a35eb6-5a54-46c1-a5af-901edbf8ef26', 'WS/BMC/1498972', '31/10/2021 - 31/10/2021', 'Working', 0, 1635678000000, 0, 1635678000000, 0, '5c4504e7-40e9-47e9-850e-1b33e699b879', '5c4504e7-40e9-47e9-850e-1b33e699b879', 1637119904010, 1637119904010, 'od.bhubaneswar'),
+('0951271a-8d37-4732-b352-4c4a14ea0baa', 'WS/BMC/1551791', '01/11/2021 - 01/11/2021', 'Working', 0, 1635677000000, 0, 1635678000000, 0, '5c4504e7-40e9-47e9-850e-1b33e699b879', '5c4504e7-40e9-47e9-850e-1b33e699b879', 1637119904010, 1637119904010, 'od.bhubaneswar');
