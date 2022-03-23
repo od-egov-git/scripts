f7b6e55569f30c31fbc70b975756e6a28a24069d
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-03-23 07:37:46 +0000

    Update fsm.sql

--- fsm.sql
+++ fsm.sql
@@ -1,5 +1,3 @@
-update eg_vehicle set registrationnumber='0D-07-AE-8338' where id='00991645-b672-4eea-8621-65102bff7e56';
-update eg_vendor set owner_id='09fece5c-c9b0-4b0c-94a4-018b7c7398be' where id='34de6921-dbff-4749-bf89-6e06cd8f792f';
-update eg_vehicle set  owner_id='09fece5c-c9b0-4b0c-94a4-018b7c7398be' where id='550fc621-7a61-49cf-9629-b11211509cef';
-update eg_vehicle set  owner_id='09fece5c-c9b0-4b0c-94a4-018b7c7398be' where id='00991645-b672-4eea-8621-65102bff7e56';
-update eg_vendor_driver set vendor_id='08c9780d-f15a-4144-988a-cf7ebb25d985' where driver_id in ('163dac50-3398-42a0-a5f9-52a711755018','459b1fe0-edc5-44fe-88a6-302b19b696c2','ced88007-8a5c-4184-ad55-440695e907c9');
+update eg_vendor set owner_id='459b1fe0-edc5-44fe-88a6-302b19b696c2' where id='08c9780d-f15a-4144-988a-cf7ebb25d985';
+update eg_vehicle set owner_id='459b1fe0-edc5-44fe-88a6-302b19b696c2' where id='3ec05ac0-e23a-423d-884a-e1171b4f48b2';
+update eg_vehicle set owner_id='459b1fe0-edc5-44fe-88a6-302b19b696c2' where id='d76aa931-6f1b-464a-8e98-f5fcedf3aeb8';
