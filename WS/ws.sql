6a8b9c7918019afe972dafcfd9eb6c07b95e206b
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-03-25 07:38:27 +0000

    Update ws.sql

--- WS/ws.sql
+++ WS/ws.sql
@@ -1,7 +1,38 @@
--- 0 Meter reading insert
-INSERT INTO public.eg_ws_meterreading
-(id, connectionno, billingperiod, meterstatus, lastreading, lastreadingdate, currentreading, currentreadingdate, consumption, createdby, lastmodifiedby, createdtime, lastmodifiedtime, tenantid) VALUES
-('b82cfaaa-0d53-4135-82c8-e4fd1663b1b0', 'WS/BMC/1504750', '01/11/2021 - 01/11/2021', 'Working', 0, 1635677000000, 0, 1635678000000, 0, '5c4504e7-40e9-47e9-850e-1b33e699b879', '5c4504e7-40e9-47e9-850e-1b33e699b879', 1637119904010, 1637119904010, 'od.bhubaneswar'),
-('5d4ebbf2-4035-470e-b490-f97ac728e92b', 'WS/BMC/1551768', '01/11/2021 - 01/11/2021', 'Working', 0, 1635677000000, 0, 1635678000000, 0, '5c4504e7-40e9-47e9-850e-1b33e699b879', '5c4504e7-40e9-47e9-850e-1b33e699b879', 1637119904010, 1637119904010, 'od.bhubaneswar'),
-('e6a35eb6-5a54-46c1-a5af-901edbf8ef26', 'WS/BMC/1498972', '31/10/2021 - 31/10/2021', 'Working', 0, 1635678000000, 0, 1635678000000, 0, '5c4504e7-40e9-47e9-850e-1b33e699b879', '5c4504e7-40e9-47e9-850e-1b33e699b879', 1637119904010, 1637119904010, 'od.bhubaneswar'),
-('0951271a-8d37-4732-b352-4c4a14ea0baa', 'WS/BMC/1551791', '01/11/2021 - 01/11/2021', 'Working', 0, 1635677000000, 0, 1635678000000, 0, '5c4504e7-40e9-47e9-850e-1b33e699b879', '5c4504e7-40e9-47e9-850e-1b33e699b879', 1637119904010, 1637119904010, 'od.bhubaneswar');
+-- Update initial meter reading if blank for migrated connection
+update eg_ws_service
+set initialmeterreading = 1
+where connection_id in (
+	select wc.connection_id from eg_ws_connection conn 
+	inner join eg_ws_service wc on wc.connection_id =conn.id 
+	where conn.applicationstatus not in ('NEW_WATER_CONNECTION','NEW_CONNECTION')
+	and conn.connectionno is not null
+	and wc.connectiontype ='Metered'
+	and wc.initialmeterreading is null);
+
+
+
+-- Update meterInstallationDate if blank for migrated connection
+update eg_ws_service
+set meterinstallationdate = connectionexecutiondate 
+where connection_id in (
+	select wc.connection_id from eg_ws_connection conn 
+	inner join eg_ws_service wc on wc.connection_id =conn.id 
+	where conn.applicationstatus not in ('NEW_WATER_CONNECTION','NEW_CONNECTION')
+	and conn.connectionno is not null
+	and wc.connectiontype ='Metered'
+	and wc.meterinstallationdate is null);
+
+
+
+	
+-- Update Metereid to default if blank for migrated connection
+update eg_ws_service
+set meterid = 'M999999' 
+where connection_id in (
+	select wc.connection_id from eg_ws_connection conn 
+	inner join eg_ws_service wc on wc.connection_id =conn.id 
+	where conn.applicationstatus not in ('NEW_WATER_CONNECTION','NEW_CONNECTION')
+	and conn.connectionno is not null
+	and wc.connectiontype ='Metered'
+	and wc.meterid is null);
+
