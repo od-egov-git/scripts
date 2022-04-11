3858f19993aa1ff2a9af635255181c64d44688a7
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-04-11 15:02:19 +0000

    Update ws.sql

--- WS/ws.sql
+++ WS/ws.sql
@@ -1,38 +1,4 @@
--- Update initial meter reading if blank for migrated connection
-update eg_ws_service
-set initialmeterreading = 1
-where connection_id in (
-	select wc.connection_id from eg_ws_connection conn 
-	inner join eg_ws_service wc on wc.connection_id =conn.id 
-	where conn.applicationstatus not in ('NEW_WATER_CONNECTION','NEW_CONNECTION')
-	and conn.connectionno is not null
-	and wc.connectiontype ='Metered'
-	and wc.initialmeterreading is null);
-
-
-
--- Update meterInstallationDate if blank for migrated connection
-update eg_ws_service
-set meterinstallationdate = connectionexecutiondate 
-where connection_id in (
-	select wc.connection_id from eg_ws_connection conn 
-	inner join eg_ws_service wc on wc.connection_id =conn.id 
-	where conn.applicationstatus not in ('NEW_WATER_CONNECTION','NEW_CONNECTION')
-	and conn.connectionno is not null
-	and wc.connectiontype ='Metered'
-	and wc.meterinstallationdate is null);
-
-
-
-	
--- Update Metereid to default if blank for migrated connection
-update eg_ws_service
-set meterid = 'M999999' 
-where connection_id in (
-	select wc.connection_id from eg_ws_connection conn 
-	inner join eg_ws_service wc on wc.connection_id =conn.id 
-	where conn.applicationstatus not in ('NEW_WATER_CONNECTION','NEW_CONNECTION')
-	and conn.connectionno is not null
-	and wc.connectiontype ='Metered'
-	and wc.meterid is null);
-
+update eg_wf_processinstance_v2
+set createdtime=1649238429000, lastmodifiedtime=1649238429000
+where businessid='WS_AP/CTC/2021-22/2220569'
+and "action"='APPROVE_FOR_CONNECTION';
