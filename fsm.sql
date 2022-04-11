477cf2ecb87243417257ad61d735be4a40e11a2e
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-04-11 00:59:45 +0000

    Update fsm.sql

--- fsm.sql
+++ fsm.sql
@@ -1,3 +1,6 @@
-update eg_vendor set owner_id='459b1fe0-edc5-44fe-88a6-302b19b696c2' where id='08c9780d-f15a-4144-988a-cf7ebb25d985';
-update eg_vehicle set owner_id='459b1fe0-edc5-44fe-88a6-302b19b696c2' where id='3ec05ac0-e23a-423d-884a-e1171b4f48b2';
-update eg_vehicle set owner_id='459b1fe0-edc5-44fe-88a6-302b19b696c2' where id='d76aa931-6f1b-464a-8e98-f5fcedf3aeb8';
+ INSERT INTO eg_wf_processinstance_v2
+ (id,tenantid,businessservice,businessid,action,status,comment,assigner,assignee,statesla,previousstatus,createdby,lastmodifiedby,createdtime,lastmodifiedtime,modulename,businessservicesla)
+VALUES
+  ('371242a8-b7ee-11ec-b909-0242ac120002','od.balasore','FSM_VEHICLE_TRIP','BLS-VT-2022-04-06-000658','DISPOSE','4a8a83c5-2d18-4edb-a4ab-8d08f10422c6',null,'cc444d5b-b7f1-4477-998b-d12da1494ef7',null,null,null,'cc444d5b-b7f1-4477-998b-d12da1494ef7','cc444d5b-b7f1-4477-998b-d12da1494ef7',1649244189455,1649244189455,'vehicle',-76847954);
+       
+update eg_vehicle_trip set applicationstatus='DISPOSED' where applicationno='BLS-VT-2022-04-06-000658';
