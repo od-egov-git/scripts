f3e4269db2fb5c46781e429c3dd50488975f25f2
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-04-15 08:44:08 +0000

    Update ws.sql

--- WS/ws.sql
+++ WS/ws.sql
@@ -1,66 +1,26 @@
--- Select queries
--- Expire all the Active bills
-select * from egbs_bill_v1 ebv
-where id in (select billid from egbs_billdetail_v1 ebv2 where businessservice ='WS' and consumercode in (
-'WS/CTC/1840334'
-)) and status = 'ACTIVE';
+-- WS/CTC/1780572
+-- Update Demand to paid
+update egbs_demand_v1 
+set ispaymentcompleted=true
+where id in ('02aae1ee-e194-4258-8a8c-9e9fb4b60625',
+'32b98b6e-a510-43ff-a642-bbec5ac38a76',
+'50ba4ab7-9091-419d-bc1a-71215318c8f2',
+'88fe1155-a659-4b74-9081-82e3a619df13',
+'a7871154-4bea-40a3-8ef6-4c73bc9df3a9',
+'ea3d6c5c-0027-4dce-a66e-7c925b6adf84');
 
--- Activate the specific bills.
-select * from egbs_bill_v1 ebv
-where id in (select billid from egbs_billdetail_v1 ebv where businessservice ='WS' and billno in (
-'BILLNO-WS-7115500'
-)) and status <> 'PAID';
+-- Update demand details
+update egbs_demanddetail_v1
+set collectionamount=taxamount
+where demandid in ('02aae1ee-e194-4258-8a8c-9e9fb4b60625',
+'32b98b6e-a510-43ff-a642-bbec5ac38a76',
+'50ba4ab7-9091-419d-bc1a-71215318c8f2',
+'88fe1155-a659-4b74-9081-82e3a619df13',
+'a7871154-4bea-40a3-8ef6-4c73bc9df3a9',
+'ea3d6c5c-0027-4dce-a66e-7c925b6adf84');
 
--- 
-select * from egbs_billdetail_v1 ebv 
-where businessservice ='WS' and billno in (
-'BILLNO-WS-7115500'
-);
-
-------*************************************------------------
--- Update queries
--- Expire all the Active bills
-update egbs_bill_v1 ebv
-set status ='EXPIRED'
-where id in (select billid from egbs_billdetail_v1 ebv2 where businessservice ='WS' and consumercode in (
-'WS/CTC/1840334'
-)) and status = 'ACTIVE';
-
--- Activate the specific bills.
-update egbs_bill_v1 ebv
-set status = 'ACTIVE'
-where id in (select billid from egbs_billdetail_v1 ebv where businessservice ='WS' and billno in (
-'BILLNO-WS-7115500'
-)) and status <> 'PAID';
-
--- set expiry date to till 15/4/2022 11:59:59
-update egbs_billdetail_v1 ebv 
-set expirydate = 1650047399000
-where businessservice ='WS' and billno in (
-'BILLNO-WS-7115500'
-);
-
-================
--- WS_AP/HNL/2021-22/2217477
--- Insert the workflow
--- Assigner keep same as previous
-INSERT INTO eg_wf_processinstance_v2 (id,tenantid,businessservice,businessid,"action",status,"comment",assigner,assignee,statesla,previousstatus,createdby,lastmodifiedby,createdtime,lastmodifiedtime,modulename,businessservicesla,rating) values
-('2040984b-76e0-4226-84af-0ec542cef2f4','od.hinjilicut','NewWS1','WS_AP/HNL/2021-22/2217477','PAY','856e04f3-9195-464b-8c07-b6ae33a4b66d',NULL,'f6b78427-9fcc-46c7-aedd-0606b9b39ae5',NULL,NULL,NULL,'f6b78427-9fcc-46c7-aedd-0606b9b39ae5','f6b78427-9fcc-46c7-aedd-0606b9b39ae5',1649923254000,1649923254000,'ws-services',258971850,NULL);
-
--- Update application status
-update eg_ws_connection
-set applicationstatus='PENDING_FOR_CONNECTION_ACTIVATION'
-where applicationno='WS_AP/HNL/2021-22/2217477' ;
-
-==================================================================
-
--- WS_AP/HNL/2021-22/2215934
--- Insert the workflow
--- Assigner keep same as previous
-INSERT INTO eg_wf_processinstance_v2 (id,tenantid,businessservice,businessid,"action",status,"comment",assigner,assignee,statesla,previousstatus,createdby,lastmodifiedby,createdtime,lastmodifiedtime,modulename,businessservicesla,rating) values
-('eb53c7bb-413c-4681-861f-a0e857bb8c11','od.hinjilicut','NewWS1','WS_AP/HNL/2021-22/2215934','PAY','856e04f3-9195-464b-8c07-b6ae33a4b66d',NULL,'db557e7c-055b-4c8f-bfbd-402786bd39c1',NULL,NULL,NULL,'db557e7c-055b-4c8f-bfbd-402786bd39c1','db557e7c-055b-4c8f-bfbd-402786bd39c1',1649923254000,1649923254000,'ws-services',258971850,NULL);
-
--- Update application status
-update eg_ws_connection
-set applicationstatus='PENDING_FOR_CONNECTION_ACTIVATION'
-where applicationno='WS_AP/HNL/2021-22/2215934' ;
+-- Expire active bills
+update egbs_bill_v1 ebv2
+set status = 'EXPIRED'
+where status ='ACTIVE' and 
+id in (select billid from egbs_billdetail_v1 ebv where consumercode = 'WS/CTC/1780572');
