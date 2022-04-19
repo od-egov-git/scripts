7d8327f2cd92d75959aafa268020fae7242cb884
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-04-19 03:38:02 +0000

    Update PT_update_demand.sql

--- PT_update_demand.sql
+++ PT_update_demand.sql
@@ -1,1 +1,1 @@
-update egbs_billdetail_v1 set expirydate = 1649116799000 where expirydate > 1649116799000 and businessservice = 'PT' and tenantid = 'od.cuttack';
+update egbs_billdetail_v1 set expirydate = 1650326399000 where expirydate > 1650326399000 and businessservice = 'PT' and tenantid = 'od.udala';
