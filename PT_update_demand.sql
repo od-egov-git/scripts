ac9ff05b1457a43b71ce4144b16df329cea20998
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-04-26 13:57:20 +0000

    Update PT_update_demand.sql

--- PT_update_demand.sql
+++ PT_update_demand.sql
@@ -1,11 +1,7 @@
 update table egbs_demanddetail_v1
-set collectionamount=-1.00
-where demandid = '0b1cdbab-7252-43d2-a6e7-f2b203e6be8f';
+set collectionamount=-509689.00
+where demandid = 'c35bf2f7-79f7-4a94-95d7-7720e5af31c5';
 
-delete from eg_wf_processinstance_v2 ewpv where businessid='PT-MT-KCN-000008' and action='APPROVE';
-delete from eg_wf_processinstance_v2 ewpv where businessid='PT-MT-PRI-000032' and action='APPROVE';
-delete from eg_wf_processinstance_v2 ewpv where businessid='PT-MT-BJR-000022' and action='APPROVE';
-delete from eg_wf_processinstance_v2 ewpv where businessid='PT-MT-SNG-000050' and action='APPROVE';
-delete from eg_wf_processinstance_v2 ewpv where businessid='PT-MT-PRI-000017' and action='APPROVE';
-delete from eg_wf_processinstance_v2 ewpv where businessid='PT-MT-RMB-000061' and action='APPROVE';
-delete from eg_wf_processinstance_v2 ewpv where businessid='PT-MT-SOR-000007' and action='APPROVE';
+update table egbs_demanddetail_v1
+set collectionamount=-97000.00
+where demandid = 'bb4ca490-c0a5-43f8-a923-a9d8466d8a1b';
