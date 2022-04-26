83c3d31a5e9687dd344b4e685031955e22d02de3
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-04-26 13:55:44 +0000

    Update PT_update_demand.sql

--- PT_update_demand.sql
+++ PT_update_demand.sql
@@ -1,1 +1,11 @@
-update egbs_billdetail_v1 set expirydate = 1650326399000 where expirydate > 1650326399000 and businessservice = 'PT' and tenantid = 'od.udala';
+update table egbs_demanddetail_v1
+set collectionamount=-1.00
+where demandid = '0b1cdbab-7252-43d2-a6e7-f2b203e6be8f';
+
+delete from eg_wf_processinstance_v2 ewpv where businessid='PT-MT-KCN-000008' and action='APPROVE';
+delete from eg_wf_processinstance_v2 ewpv where businessid='PT-MT-PRI-000032' and action='APPROVE';
+delete from eg_wf_processinstance_v2 ewpv where businessid='PT-MT-BJR-000022' and action='APPROVE';
+delete from eg_wf_processinstance_v2 ewpv where businessid='PT-MT-SNG-000050' and action='APPROVE';
+delete from eg_wf_processinstance_v2 ewpv where businessid='PT-MT-PRI-000017' and action='APPROVE';
+delete from eg_wf_processinstance_v2 ewpv where businessid='PT-MT-RMB-000061' and action='APPROVE';
+delete from eg_wf_processinstance_v2 ewpv where businessid='PT-MT-SOR-000007' and action='APPROVE';
