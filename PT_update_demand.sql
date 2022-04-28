c2ef67d7f132cd6c958181723ac1b63a7c735d03
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-04-28 09:48:12 +0000

    Update PT_update_demand.sql

--- PT_update_demand.sql
+++ PT_update_demand.sql
@@ -1,7 +1,3 @@
 update table egbs_demanddetail_v1
-set collectionamount=-509689.00
-where demandid = 'c35bf2f7-79f7-4a94-95d7-7720e5af31c5';
-
-update table egbs_demanddetail_v1
-set collectionamount=-97000.00
-where demandid = 'bb4ca490-c0a5-43f8-a923-a9d8466d8a1b';
+set collectionamount=-1.00
+where id= '0b1cdbab-7252-43d2-a6e7-f2b203e6be8f';
