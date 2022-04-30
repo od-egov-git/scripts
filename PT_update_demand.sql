1b5fbc645599e9de7bac04bc4ca42907a2312847
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-04-30 05:23:52 +0000

    Update PT_update_demand.sql

--- PT_update_demand.sql
+++ PT_update_demand.sql
@@ -1,3 +1,3 @@
-update table egbs_demanddetail_v1
-set collectionamount=-1.00
-where id= '0b1cdbab-7252-43d2-a6e7-f2b203e6be8f';
+delete from eg_wf_processinstance_v2 ewpv where businessid='PT-AS-PRI-2022-04-25-1137759' and action='APPROVE';
+
+update egbs_billdetail_v1 set expirydate = 1651276799000 where expirydate > 1651276799000 and businessservice = 'PT';
