9a81cca30e01b2a7b56a36b1779a9c12b34176dd
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2022-05-06 07:02:53 +0000

    Update PT_update_demand.sql

--- PT_update_demand.sql
+++ PT_update_demand.sql
@@ -1,23 +1,33 @@
-update table egbs_demanddetail_v1
-set collectionamount=-5400.00
-where id = '5e54038a-66e7-4242-9036-03a127b5fe26';
+update egbs_demanddetail_v1
+set collectionamount=taxamount where demandid in (
+select id from egbs_demand_v1 edv where id in (
+select demandid from egbs_billdetail_v1 ebd where billid in (
+select billid from egcl_paymentdetail ep where paymentid in (
+select id from egcl_payment
+where totaldue = totalamountpaid
+and instrumentstatus <> 'CANCELLED'
+and  to_timestamp(createdtime/1000)::date > '2022-04-11'
+) and due = amountpaid and businessservice = 'PT'
+)
+) and ispaymentcompleted = false and status = 'ACTIVE'
+) and taxheadcode <> 'PT_ADVANCE_CARRYFORWARD' and collectionamount <> taxamount;
 
-update table egbs_demanddetail_v1
-set collectionamount=-1400.00
-where id = '241389b0-1052-4dab-a322-49da3c3ae404';
 
-update table egbs_demanddetail_v1
-set collectionamount=-1400.00
-where id = '5bfce194-1ef2-46fd-a64e-b77395136c88';
-
-update table egbs_demanddetail_v1
-set collectionamount=-7000.00
-where id = '241aa5d2-3796-41bd-a21c-f6e2175481df';
-
-update table egbs_demanddetail_v1
-set collectionamount=-31054.00
-where id = '24ea1f6a-47d6-4695-a47b-a747a8d32bca';
-
-update egbs_billdetail_v1 set expirydate = 1649116799000 where expirydate > 1649116799000 and businessservice = 'PT' and consumercode='PT-PRI-558239';
-
-delete from eg_wf_processinstance_v2 ewpv where businessid='PT-MT-PRI-000016' and action in ('VERIFY', 'FORWARD', 'APPROVE');
+update egbs_demand_v1 edv
+set ispaymentcompleted = true
+where edv.id in (
+select demandid from egbs_billdetail_v1 ebd where billid in (
+select billid from egcl_paymentdetail ep where paymentid in (
+select id from egcl_payment
+where totaldue = totalamountpaid
+and instrumentstatus <> 'CANCELLED'
+and  to_timestamp(createdtime/1000)::date > '2022-04-11'
+) and due = amountpaid and businessservice = 'PT'
+) except
+(
+select distinct demandid
+from egbs_demanddetail_v1
+where taxheadcode = 'PT_ADVANCE_CARRYFORWARD'
+and  to_timestamp(createdtime/1000)::date > '2022-04-11'
+)
+) and ispaymentcompleted = false and status = 'ACTIVE'
